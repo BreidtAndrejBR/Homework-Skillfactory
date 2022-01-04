7f72987ca76f50306af4d97ae7d5aaeb13556cb9
Andrej Breidt <33092273+BreidtAndrejBR@users.noreply.github.com>
2022-01-04 12:33:39 +0100

    My program
    This is the Program!

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;                                                // Открытие пространство имён класса "Console"                                                
+
+namespace HomeworkHelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)                                     // Старт программы
+        {
+            // Homework N. 1
+            WriteLine("Hello World !!!");
+            ReadKey();
+
+            // Homework N. 2
+            WriteLine("...please, press [Spacekey] to continue...");        // Вывод информации на экран 
+            ReadKey();                                                      // Метод ожидания нажатия клавишы 
+            Write("Hello"); 
+            ReadKey();
+            Write("World");
+            ReadKey();
+            Write("!!!");
+            WriteLine("\n...please, press [ENTER] to exit...");
+            ReadKey().Key.Equals(ConsoleKey.Enter);                         // Метод сравнивания нажатой клавишы с заданной клавишои [ENTER]
+
+
+            Read();                                                         // Последние указание программы перед закрытием
+
+
+        }
+    }
+}
