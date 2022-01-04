2be1a9a69b2515994bf28136a3f48c018094f843
Andrej Breidt <33092273+BreidtAndrejBR@users.noreply.github.com>
2022-01-04 12:55:24 +0100

    Homework 1.4
    Домашнее задание 1,4
    - Вывод на экран консоли информацию "Hello World !!!"
    - Вывод на экран консоли информацию в последовательном порядке "Hello" + "World" +  "!!!"

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using static System.Console;                                                // Открытие пространство имён класса "Console"                                                
 
+// Homework 1.4
 namespace HomeworkHelloWorldConsoleApp
 {
     class Program
     {
         static void Main(string[] args)                                     // Старт программы
         {
-            // Homework N. 1
+            // Task N. 1
             WriteLine("Hello World !!!");
             ReadKey();
 
-            // Homework N. 2
-            WriteLine("...please, press [Spacekey] to continue...");        // Вывод информации на экран 
+            // Task N. 2
+            WriteLine("...please, press [Spacekey] to continue...");        // Вывод информации на экран  (переносит следующую информацию на новую строку)
             ReadKey();                                                      // Метод ожидания нажатия клавишы 
-            Write("Hello"); 
+            Write("Hello");                                                 // Вывод информации на экран последовательно на одной строке
             ReadKey();
             Write("World");
             ReadKey();
